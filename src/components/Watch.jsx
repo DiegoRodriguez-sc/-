d963e8e9ec0a1cf624f00e17d33337c0f8fe65c3
diegorodriguez <diego.rodriguez@teledoc.cl>
2022-08-14 22:55:03 -0300

    watch finished

--- src/components/Watch.jsx
+++ src/components/Watch.jsx
@@ -1,44 +1,49 @@
 import React, { useEffect, useState } from 'react'
+import { getTime } from '../helpers/helper';
 
 const Watch = () => {
 
-    const [value, setValue] = useState(30)
+    const dateBirthday = new Date("Fri Jan 13 2023");
 
+    const [value, setValue] = useState({});
     useEffect(() => {
       const timer = setTimeout(() => {
-        setValue((v) => (v <= 0 ? 30 : v - 1))
+       
+        const currenTime = getTime(dateBirthday);
+        setValue(currenTime);
+
+        if (currenTime.time <= 1) {
+            clearInterval(timer);
+        }
+
       }, 1000)
-  
-      return () => {
-        clearTimeout(timer)
-      }
+
     }, [value])
 
-    
   return (
     <div className="App">
     <div className="flex gap-5">
       <div>
-        <span className="countdown font-mono text-4xl">
-          <span style={{"--value":"10;"}}></span>
+        <span className="countdownCustom font-mono text-4xl">
+          <span style={{"--value":value.days}}></span>
         </span>
         days
       </div>
       <div>
         <span className="countdown font-mono text-4xl">
-          <span style={{"--value":10}}></span>
+          <span style={{"--value":value.hours}}></span>
         </span>
         hours
       </div>
       <div>
         <span className="countdown font-mono text-4xl">
-          <span style={{"--value":10}}></span>
+          <span style={{"--value":value.minutes}}></span>
         </span>
         min
       </div>
       <div>
         <span className="countdown font-mono text-4xl">
-          <span style={{"--value":value}}></span>
+          <span style={{"--value":value.seconds}}></span>
         </span>
         sec
       </div>
