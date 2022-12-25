61e924b6e0769c7d48a1169c859d24e95f030a7d
diego rodriguez <diego.rodriguez@teledoc.cl>
2022-12-24 23:51:34 -0300

    gift added

--- src/components/Christmas/DashBoardChristmas.jsx
+++ src/components/Christmas/DashBoardChristmas.jsx
@@ -1,8 +1,20 @@
 import React from "react";
+import img from "../../helpers/img";
 import Watch from "../Watch";
 import Background from "./Background";
 
 const DashBoardChristmas = () => {
+
+  const gift = () =>{
+    var source = img[Math.floor(Math.random() * 5) + 1];
+    var a = document.createElement('a');
+    a.download = true;
+    a.target = '_blank';
+    a.href= source;
+    a.click();
+  }
+
+ 
   
   return (
       <div className="hero-content text-center">
@@ -11,6 +23,8 @@ const DashBoardChristmas = () => {
           <p className="pt-6">time left until your birthday 🎂</p>
           <p className="pb-6">Merry christmas 🎄🎅</p>
           <Watch />
+          <br />
+          <button onClick={gift}>🎁</button>
         <Background id="tsparticles" />
         </div>
       </div>
---
+++ src/helpers/img.js
@@ -0,0 +1,9 @@
+const img = {
+    1:"https://res.cloudinary.com/dgxoj05dn/image/upload/v1671936274/1_wkmn6g.jpg",
+    2:"https://res.cloudinary.com/dgxoj05dn/image/upload/v1671936274/2_uolfjk.jpg",
+    3:"https://res.cloudinary.com/dgxoj05dn/image/upload/v1671936273/3_dw2ynh.jpg",
+    4:"https://res.cloudinary.com/dgxoj05dn/image/upload/v1671936273/4_swmht2.jpg",
+    5:"https://res.cloudinary.com/dgxoj05dn/image/upload/v1671936273/5_sedqyv.jpg",
+}
+
+export default img;
